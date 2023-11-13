55556bc483016f60fd5bfc3a2158afced01aa671
ildar.saifeev <ildars.saifeev@gmail.com>
2023-11-14 03:53:13 +0400

    fix assertion in UserControllerTest - due to changing order of users in responses, tests should take that into account

--- src/test/java/com/ildar/event/controller/UserControllerTest.java
+++ src/test/java/com/ildar/event/controller/UserControllerTest.java
@@ -10,6 +10,8 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static com.ildar.event.TestData.USER_DTOS;
+import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.hamcrest.Matchers.notNullValue;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -40,11 +42,10 @@ class UserControllerTest {
 
         mockMvc.perform(get("/users"))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$[0].userId").isString())
-                .andExpect(jsonPath("$[0].username").value(USER_DTOS.get(0).username()))
-                .andExpect(jsonPath("$[0].email").value(USER_DTOS.get(0).email()))
-                .andExpect(jsonPath("$[1].userId").isString())
-                .andExpect(jsonPath("$[1].username").value(USER_DTOS.get(1).username()))
-                .andExpect(jsonPath("$[1].email").value(USER_DTOS.get(1).email()));
+                .andExpect(jsonPath("$[*].userId", containsInAnyOrder(notNullValue(), notNullValue())))
+                .andExpect(jsonPath("$[*].username",
+                        containsInAnyOrder(USER_DTOS.get(0).username(), USER_DTOS.get(1).username())))
+                .andExpect(jsonPath("$[*].email",
+                        containsInAnyOrder(USER_DTOS.get(0).email(), USER_DTOS.get(1).email())));
     }
 }
